c43462e0aa3fcbb39fd4376d66ff7930e9ff9cbe
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2023-08-11 10:04:09 +0900

    update: テキスト整形処理を調整

--- src/createProperties.ts
+++ src/createProperties.ts
@@ -18,7 +18,7 @@ export default async (item: FeedEntry) => {
 
   // URL部分を短縮
   let text = rt.text;
-  let targets: { key: string; link: string }[] = [];
+  let targets: { link: string }[] = [];
   rt.facets?.forEach((v) => {
     if (
       v.features[0]['$type'] === 'app.bsky.richtext.facet#link' &&
@@ -30,42 +30,49 @@ export default async (item: FeedEntry) => {
           ? link
           : splitter.splitGraphemes(link).slice(0, 27).join('') + '...';
       text = text.replace(link, key);
-      targets = [...targets, { key, link }];
+      targets = [...targets, { link }];
     }
   });
 
   // 300文字を超える場合は、300文字になるように切り詰める
   const max = 300;
-  const isOverLength = splitter.countGraphemes(text) > max;
   const shortenedLink =
     splitter.countGraphemes(link) <= 27
       ? link
       : splitter.splitGraphemes(link).slice(0, 27).join('') + '...';
+  targets = [...targets, { link }];
+  const isOverLength =
+    splitter.countGraphemes(text) >
+    max - splitter.countGraphemes(`\n\n${shortenedLink}`);
   if (isOverLength) {
-    const ellipsis = `...\n\n${shortenedLink}`;
-    const cnt = max - splitter.countGraphemes(ellipsis);
+    const ellipsis = `...`;
+    const cnt =
+      max - splitter.countGraphemes(`${ellipsis}\n\n${shortenedLink}`);
     const shortenedText = splitter.splitGraphemes(text).slice(0, cnt).join('');
     text = `${shortenedText}${ellipsis}`;
   }
+  // 本文の最後に短縮したURLを追加
+  text = `${text}\n\n${shortenedLink}`;
 
   rt = new RichText({ text });
   await rt.detectFacets(agent);
 
-  // 短縮したURLのリンク先を元のURLに置き換え
-  rt.facets?.forEach((v, i) => {
+  // 短縮したURLのリンク先を元のURLに置き換える
+  const facets = rt.facets || [];
+  facets.forEach((v, i) => {
     if (
       v.features[0]['$type'] === 'app.bsky.richtext.facet#link' &&
       typeof v.features[0].uri === 'string' &&
       targets[i]?.link
     ) {
       // 文字数超過の場合は、最後のリンクは元URLになるはず
-      v.features[0].uri =
-        isOverLength && rt.facets && i === rt.facets.length - 1
-          ? link
-          : targets[i].link;
+      const uri =
+        isOverLength && i === facets.length - 1 ? link : targets[i].link;
+      v.features[0].uri = uri;
+      v.index.byteEnd = v.index.byteStart + splitter.countGraphemes(uri);
     }
   });
 
   console.log('success createProperties');
-  return { text: rt.text, facets: rt.facets, title, link };
+  return { text: rt.text, facets, title, link };
 };
